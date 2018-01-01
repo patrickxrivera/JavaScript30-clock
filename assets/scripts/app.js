cdb0b0fef4e69b61a25f8338bcf4e747bb4f948f
Patrick <patrick.x.rivera@gmail.com>
2017-12-31 22:29:48 -0500

    Finalized v2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,71 +1,16 @@
-// /*
-// ====== Variable Declarations ======
-// */
-//
-// const currentTime = new Date();
-//
-// var hourHandAngle = getAngle(currentTime, 'hour');
-// var minuteHandAngle = getAngle(currentTime, 'minute');
-// var secondHandAngle =getAngle(currentTime, 'second');
-//
-// // Degree increment out of 360 each second by hour, minute, and second
-// const hourHandIncrement = 8 / 1000;
-// const minuteHandIncrement = 1 / 10;
-// const secondHandIncrement = 6;
-//
-// const hourHand = document.querySelector('.hour-hand')
-// const minuteHand = document.querySelector('.min-hand')
-// const secondHand = document.querySelector('.second-hand');
-//
-// /*
-// ======= Functions =======
-// */
-//
-// function getAngle(currentTime, angleType) {
-//   if (angleType === 'hour') {
-//     return currentTime.getHours() / 24 * 360
-//   }
-//   else if (angleType === 'minute') {
-//     return currentTime.getMinutes() / 60 * 360;
-//   }
-//   else {
-//     return currentTime.getSeconds() / 60 * 360;
-//   }
-// }
-//
-// function rotateHand(hand, angle) {
-//   return hand.style.transform = `rotate(${angle}deg)`;
-// }
-//
-// function getDegreeRotation(angle, increment) {
-//   return angle + increment;
-// }
-//
-// (function rotateHands() {
-//   hourHandAngle = getDegreeRotation(hourHandAngle, hourHandIncrement);
-//   minuteHandAngle = getDegreeRotation(minuteHandAngle, minuteHandIncrement);
-//   secondHandAngle = getDegreeRotation(secondHandAngle, secondHandIncrement);
-//
-//   rotateHand(hourHand, hourHandAngle);
-//   rotateHand(minuteHand, minuteHandAngle);
-//   rotateHand(secondHand, secondHandAngle);
-//
-//   setTimeout(rotateHands, 1000);
-// })();
-
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
 function getTime(currentTime, handType) {
-  if (handType === 'hour') { return currentTime.getHour() }
-  if (handType === 'minute') { return currentTime.getMinute() }
+  if (handType === 'hour') { return currentTime.getHours() }
+  if (handType === 'minute') { return currentTime.getMinutes() }
   if (handType === 'second') { return currentTime.getSeconds() }
 }
 
 function getDegree(time, handType) {
-  if (handType === 'hour') { return (time / 43200) * 360 + 90 }
-  if (handType === 'minute') { return (time / 3600) * 360 + 90 }
+  if (handType === 'hour') { return (time / 12) * 360 + 90 }
+  if (handType === 'minute') { return (time / 60) * 360 + 90 }
   if (handType === 'second') {return (time / 60) * 360 + 90 }
 }
 
@@ -76,9 +21,16 @@ function setRotation(hand, degree) {
 function rotateHands() {
   const currentTime = new Date();
 
+  const hours = getTime(currentTime, 'hour');
+  const hoursDegree = getDegree(hours, 'hour');
+  setRotation(hourHand, hoursDegree);
+
+  const minutes = getTime(currentTime, 'minute');
+  const minutesDegree = getDegree(minutes, 'minute')
+  setRotation(minuteHand, minutesDegree);
+
   const seconds = getTime(currentTime, 'second');
   const secondsDegree = getDegree(seconds, 'second')
-
   setRotation(secondHand, secondsDegree);
 }
 
