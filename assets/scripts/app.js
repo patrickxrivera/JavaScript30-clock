b1d4e4014e6798ae465d257a8bfd2def6dead753
Patrick <patrick.x.rivera@gmail.com>
2017-12-31 22:39:22 -0500

    Cleaned up getDegrees function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,36 +2,26 @@ const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function getTime(currentTime, handType) {
-  if (handType === 'hour') { return currentTime.getHours() }
-  if (handType === 'minute') { return currentTime.getMinutes() }
-  if (handType === 'second') { return currentTime.getSeconds() }
-}
-
-function getDegree(time, handType) {
-  if (handType === 'hour') { return (time / 12) * 360 + 90 }
-  if (handType === 'minute') { return (time / 60) * 360 + 90 }
-  if (handType === 'second') {return (time / 60) * 360 + 90 }
+function getDegree(time, max) {
+  return (time / max) * 360 + 90;
 }
 
 function setRotation(hand, degree) {
   hand.style.transform = `rotate(${degree}deg)`;
 }
 
-function rotateHands() {
-  const currentTime = new Date();
+setInterval(() => {
+  const now = new Date();
 
-  const hours = getTime(currentTime, 'hour');
-  const hoursDegree = getDegree(hours, 'hour');
+  const hours = now.getHours();
+  const hoursDegree = getDegree(hours, 24);
   setRotation(hourHand, hoursDegree);
 
-  const minutes = getTime(currentTime, 'minute');
-  const minutesDegree = getDegree(minutes, 'minute')
+  const minutes = now.getMinutes();
+  const minutesDegree = getDegree(minutes, 60)
   setRotation(minuteHand, minutesDegree);
 
-  const seconds = getTime(currentTime, 'second');
-  const secondsDegree = getDegree(seconds, 'second')
+  const seconds = now.getSeconds();
+  const secondsDegree = getDegree(seconds, 60)
   setRotation(secondHand, secondsDegree);
-}
-
-setInterval(rotateHands, 1000);
+}, 1000); // 1000ms === 1s
